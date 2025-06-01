7bedda4701a2bb4c11facdec651fa3b5ace18dd7
MDAlviano <mdalviano.21122@gmail.com>
2025-06-01 13:55:44 +0700

    style: add LoginScreen & RegisterScreen

--- D2_Modul2/app/src/main/java/com/alvin/d2_modul2/ui/activity/RegisterScreen.kt
+++ D2_Modul2/app/src/main/java/com/alvin/d2_modul2/ui/activity/RegisterScreen.kt
@@ -1,14 +1,15 @@
-package com.alvin.d2_modul2
+package com.alvin.d2_modul2.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alvin.d2_modul2.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_screen)
     }
 }
