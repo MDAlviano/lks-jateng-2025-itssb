9b1845c45ffb34f5e35a18e9d263aa34e8e61b2f
MDAlviano <mdalviano.21122@gmail.com>
2025-06-13 20:08:00 +0700

    add: add constants to store all const variable

---
+++ D2_Modul2/app/src/main/java/com/alvin/d2_modul2/constants/Constants.kt
@@ -0,0 +1,10 @@
+package com.alvin.d2_modul2.constants
+
+object Constants {
+
+    const val BASE_URL = "http://localhost:5000"
+    const val PREF_NAME = "GaweSession"
+    const val KEY_AUTH_TOKEN = "authToken"
+    const val KEY_LOGGED_USER = "loggedUser"
+
+}
