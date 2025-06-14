ca54a2b081a00eeb057607d680ab62053d35641d
MDAlviano <mdalviano.21122@gmail.com>
2025-06-14 19:39:32 +0700

    add: get job list from api

--- D2_Modul2/app/src/main/java/com/alvin/d2_modul2/repository/JobRepository.kt
+++ D2_Modul2/app/src/main/java/com/alvin/d2_modul2/repository/JobRepository.kt
@@ -1,7 +1,9 @@
 package com.alvin.d2_modul2.repository
 
 import com.alvin.d2_modul2.http.HttpHandler
+import com.alvin.d2_modul2.model.Jobs
 import com.alvin.d2_modul2.util.JsonParser
+import org.json.JSONObject
 
 object JobRepository {
 
@@ -13,6 +15,23 @@ object JobRepository {
         )
     }
 
+    fun getJobs(endpoint: String): List<Jobs> {
+        val jobs = mutableListOf<Jobs>()
+        val response = HttpHandler.getRequest(
+            endpoint = endpoint
+        )
 
+        try {
+            val jsonObject = JSONObject(response)
+            val data = jsonObject.getJSONArray("data")
+            jsonParser.parseJobFromJson(
+                jobs = jobs,
+                jsonArray = data
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return jobs
+    }
 
 }
