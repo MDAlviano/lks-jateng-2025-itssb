f8bd33a6157951fc30ee46f507e4f4bcb35f60f7
MDAlviano <mdalviano.21122@gmail.com>
2025-07-01 15:34:56 +0700

    feat: purchase course with coupon

---
+++ D2_Modul3/D2_Modul3/Controllers/CouponController.cs
@@ -0,0 +1,70 @@
+﻿using D2_Modul3.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D2_Modul3.Controllers
+{
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private ApplicationDbContext dbContext;
+
+        public CouponController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("/coupons")]
+        public IActionResult GetCoupons(
+        [FromQuery] string? couponCode,
+        [FromQuery] int page = 1,
+        [FromQuery] int size = 10)
+        {
+            if (page < 1) page = 1;
+            if (size < 1) size = 10;
+
+            var query = dbContext.Coupons.AsQueryable();
+            if (!string.IsNullOrEmpty(couponCode))
+            {
+                query = query.Where(q => q.code.Contains(couponCode));
+            }
+
+            if (page < 0)
+            {
+                return BadRequest(new
+                {
+                    message = "Validation error: 'page' must be a positive integer."
+                });
+            }
+
+            int totalCoupons = query.Count();
+            int totalPages = (int)Math.Ceiling((double)totalCoupons / size);
+
+            var coupons = query
+                .Skip((page - 1) * size)
+                .Take(size)
+                .Select(c => new
+                {
+                    couponId = c.id,
+                    couponCode = c.code,
+                    discountValue = c.discount_pct,
+                    expiryDate = c.expiry_date,
+                    quota = c.quota
+                }).ToList();
+
+            return Ok(new
+            {
+                data = coupons,
+                pagination = new
+                {
+                    page = page,
+                    size = size,
+                    totalPage = totalPages
+                }
+            });
+
+        }
+
+    }
+}
--- D2_Modul3/D2_Modul3/Controllers/CourseController.cs
+++ D2_Modul3/D2_Modul3/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using D2_Modul3.Data;
+using D2_Modul3.Entities;
 using D2_Modul3.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -19,19 +20,25 @@ namespace D2_Modul3.Controllers
         [HttpGet]
         [Route("/courses")]
         public IActionResult GetCourses(
-    [FromQuery] string? title,
-    [FromQuery] string? sort = "desc",
-    [FromQuery] int page = 1,
-    [FromQuery] int size = 10)
+        [FromQuery] string? title,
+        [FromQuery] int page = 1,
+        [FromQuery] int size = 10)
         {
             if (page < 1) page = 1;
             if (size < 1) size = 10;
 
             var query = dbContext.Courses.AsQueryable();
-
             if (!string.IsNullOrEmpty(title))
             {
                 query = query.Where(c => c.title.Contains(title));
+            }
+
+            if (page < 0)
+            {
+                return BadRequest(new
+                {
+                    message = "Validation error: 'page' must be a positive integer."
+                });
             }
 
             int totalCourses = query.Count();
@@ -65,8 +72,15 @@ namespace D2_Modul3.Controllers
         [Route("/courses/{courseId:int}")]
         public IActionResult GetCourseDetails(int courseId)
         {
-            var selectedCourse = dbContext.Courses.FirstOrDefault(c => c.id.Equals(courseId));
+            if (courseId.GetType() != typeof(int))
+            {
+                return BadRequest(new
+                {
+                    message = "Validation error: courseId must be numeric."
+                });
+            }
 
+            var selectedCourse = dbContext.Courses.FirstOrDefault(c => c.id.Equals(courseId));
             if (selectedCourse == null)
             {
                 return NotFound(new
@@ -93,32 +107,89 @@ namespace D2_Modul3.Controllers
             });
         }
 
-        //[HttpPost]
-        //[Authorize]
-        //[Route("/courses/{courseId:int}/purchase")]
-        //public IActionResult PurchaseCourseWithCoupon(int courseId, [FromQuery] int userId, [FromBody] PurchaseDto purchaseDto)
-        //{
-        //    if(!ModelState.IsValid)
-        //    {
-        //        return BadRequest(new
-        //        {
-        //            message = "Invalid request body."
-        //        });
-        //    }
+        [HttpPost]
+        [Authorize]
+        [Route("/courses/{courseId:int}/purchase")]
+        public IActionResult PurchaseCourseWithCoupon(
+            int courseId,
+            [FromQuery] int userId,
+            [FromBody] PurchaseDto purchaseDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    message = "Invalid request body."
+                });
+            }
 
-        //    var user = User.Identity?.IsAuthenticated;
+            var user = User.Identity?.IsAuthenticated;
+            if (user == null)
+            {
+                return Unauthorized(new
+                {
+                    message = "Please login/register to purchase the course"
+                });
+            }
 
-        //    if(user == null)
-        //    {
-        //        return Unauthorized(new
-        //        {
-        //            message = "Please login/register to purchase the course"
-        //        });
-        //    }
+            var selectedCourse = dbContext.Courses.FirstOrDefault(c => c.id.Equals(courseId));
+            var selectedCoupon = dbContext.Coupons.FirstOrDefault(c => c.code.Contains(purchaseDto.coupon_code));
+            if (selectedCourse is null)
+            {
+                return NotFound(new
+                {
+                    message = "Course not found."
+                });
+            }
 
+            if (selectedCoupon is null)
+            {
+                return NotFound(new
+                {
+                    message = "Coupon not found."
+                });
+            }
 
+            if (DateTime.Now > selectedCoupon.expiry_date || selectedCoupon.quota <= 0)
+            {
+                return BadRequest(new
+                {
+                    message = "Validation error: coupon code has expired or quota exceeded."
+                });
+            }
 
-        //}
+            var newPrice = (double)(selectedCourse.price - selectedCoupon.discount_pct);
+            var newPurchase = new Purchases
+            {
+                user_id = userId,
+                course_id = courseId,
+                coupon_id = selectedCoupon.id,
+                price_paid = newPrice,
+                payment_method = purchaseDto.payment_method,
+                purchased_at = DateTime.Now
+            };
+
+            selectedCoupon.quota -= 1;
+            dbContext.Purchases.Add(newPurchase);
+            dbContext.SaveChanges();
+
+            return Ok(new
+            {
+                message = "Course purchased successfully",
+                data = new
+                {
+                    purchaseId = newPurchase.id,
+                    courseId = courseId,
+                    userId = userId,
+                    purchaseDate = newPurchase.purchased_at,
+                    paymentMethod = newPurchase.payment_method,
+                    originalPrice = selectedCourse.price,
+                    discountApplied = selectedCoupon.discount_pct,
+                    paidAmount = newPrice
+                }
+            });
+
+        }
 
     }
 }
